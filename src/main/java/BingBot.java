47dc74de0c71a84798ec282809d7736e9c32a78d
JeromeG89 <jeromegoh89@gmail.com>
2025-08-19 16:42:49 +0800

    added marking of task

--- src/main/java/BingBot.java
+++ src/main/java/BingBot.java
@@ -10,13 +10,13 @@ public class BingBot {
                 + "\n Hello! I'm %s\n What can I do for you?\n" + line
                 + "\n Bye. Hope to see you again soon!\n" + line, name);
         System.out.println(greet);
-        List<String> stored = new ArrayList<>();
+        List<Task> stored = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
-
+            String[] parts = input.split(" ");
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to never see you again >:[");
@@ -28,8 +28,19 @@ public class BingBot {
                     System.out.println((i + 1) + ". " + stored.get(i));
                 }
                 System.out.println(line);
+            } else if (parts[0].equals("unmark")  && parts.length >= 2 && Integer.parseInt(parts[1]) <= stored.size()) {
+                int markIndex = Integer.parseInt(parts[1]) - 1;
+                stored.get(markIndex).unmark();
+                System.out.println("Ok i unmarked liao");
+                System.out.println(stored.get(markIndex));
+            } else if (parts[0].equals("mark") && parts.length >= 2 && Integer.parseInt(parts[1]) <= stored.size()) {
+                int markIndex = Integer.parseInt(parts[1]) - 1;
+                stored.get(markIndex).mark();
+                System.out.println("Ok i marked liao");
+                System.out.println(stored.get(markIndex));
             } else {
-                stored.add(input);
+                Task inputTask = new Task(input);
+                stored.add(inputTask);
                 System.out.println(line);
                 System.out.println("added: " + input);
                 System.out.println(line);
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    boolean done;
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public String toString() {
+        return done
+            ? "[X] " + this.name
+            : "[] " + this.name;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+}
